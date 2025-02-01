25c7c1f2e9627ada289420373f1ea16843394787
qhdltmwhs <75594785+qhdltmwhs@users.noreply.github.com>
2025-02-01 16:55:12 +0900

    docker-compose.yml first run

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
 public class UserController {
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot!";
     }
 
     @GetMapping("/users")
